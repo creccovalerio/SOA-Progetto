bdb8d76930986b4cd04f8f234fa3906c90af863e
creccovalerio <79303724+creccovalerio@users.noreply.github.com>
2024-09-07 16:59:14 +0200

    Update utils.h

--- reference-monitor/lib/include/utils.h
+++ reference-monitor/lib/include/utils.h
@@ -7,7 +7,7 @@ char *retrieve_dir(char *path);
 char *retrieve_exe_path(struct path path_struct);
 char *retrieve_pwd(void);
 char *cipher_password(char *password, char *salt);
-char *generate_hash(const unsigned char *data);
+char *generate_hash(const unsigned char *cmd_path);
 
 
 #endif
